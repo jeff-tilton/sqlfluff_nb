513640fe7d7b576234105600141df07fe87b9349
jeff-tilton <89001004076157@FEDIDCARD.GOV>
2022-11-30 12:09:32 -0700

    added argparse

--- sqlfluff_nb/main.py
+++ sqlfluff_nb/main.py
@@ -1,11 +1,22 @@
+import argparse
 import json
 import logging
-from typing import Optional
+import logging.config
+import sys
+from copy import deepcopy
+from typing import Optional, Sequence
 
 import sqlfluff
 import typer
 
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "disable_existing_loggers": True,
+    }
+)
 LOGGER = logging.getLogger(__name__)
+FORMAT = "%(levelname)s - %(asctime)s - %(name)s - %(message)s"
 
 
 def read_nb(filename: str) -> dict:
@@ -54,8 +65,10 @@ def get_source(cell: dict) -> list:
 
 def fix_sql(source: list, **kwargs) -> str:
     current_source = " ".join(source).replace("\r", " ")
+    # LOGGER.info(f"{current_source}")
     try:
         new_source = sqlfluff.fix(current_source, **kwargs)
+        LOGGER.info(f"{new_source}")
     except Exception as e:
         LOGGER.error(f"Error fixing {current_source}", exc_info=True)
         raise e
@@ -68,6 +81,7 @@ def fix_nb(nb: dict, **kwargs) -> dict:
         if cell_is_code(cell) and language_is_sql(cell):
             try:
                 source = get_source(cell)
+                LOGGER.debug(f"{source}")
             except Exception as e:
                 LOGGER.warning("Could not get source")
                 continue
@@ -101,24 +115,42 @@ def callback():
 
 def format_file(filename: str, **kwargs) -> int:
     current_nb = read_nb(filename)
-    new_nb = fix_nb(current_nb, **kwargs)
+    new_nb = fix_nb(deepcopy(current_nb), **kwargs)
+
     if current_nb == new_nb:
+        LOGGER.info(f"{filename} did not change")
         return 0
     nb_string = json.dumps(new_nb, indent=4)
     with open(filename, "w", encoding="UTF-8") as f:
         f.write(nb_string)
+        LOGGER.info(f"Writing changes to {filename}")
     return 1
 
 
 @app.command(context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
-def fix(ctx: typer.Context, filename: Optional[str] = typer.Argument(None)):
+def fix(ctx: typer.Context, filename: Optional[str] = typer.Argument(None), level: int = 10):
+    logging.basicConfig(stream=sys.stderr, level=level, format=FORMAT)
     kwargs = get_kwargs(ctx)
-    print(kwargs)
     try:
         format_file(filename, **kwargs)
     except:
-        LOGGER.error(f"{filename} did not format")
+        LOGGER.error(f"{filename} did not format", exc_info=True)
+
+
+def main(argv: Sequence[str] = None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", nargs="*")
+    parser.add_argument("--dialect", default="ansi")
+    parser.add_argument("--rules", default=None, nargs="*")
+    parser.add_argument("--exclude_rules", default=None, nargs="*")
+    parser.add_argument("--config_path", default=None)
+    args = parser.parse_args(argv)
+    kwargs = {k: v for k, v in vars(args).items() if k != "filenames"}
+    retv = 0
+    for filename in args.filenames:
+        retv |= format_file(filename, **kwargs)
+    return retv
 
 
 if __name__ == "__main__":
-    SystemExit(app())
+    raise SystemExit(main())
